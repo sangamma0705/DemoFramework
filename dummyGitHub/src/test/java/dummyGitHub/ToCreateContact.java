4e57bec3365382f609bb5db6b596d9c0d6bfcb9f
code with srnagur <acchinagur@gmail.com>
2024-07-19 09:02:24 +0530

    first push

---
+++ dummyGitHub/src/test/java/dummyGitHub/ToCreateContact.java
@@ -0,0 +1,11 @@
+package dummyGitHub;
+
+public class ToCreateContact {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("successufuuly");
+
+	}
+
+}
